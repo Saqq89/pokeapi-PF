ccfe44e869aa1000de1a0834543aa841f48eac62
Edo <seq81@hotmail.com>
2023-05-20 19:37:38 -0600

    Cambio 21 minutos

--- src/app/components/poke-table/poke-table.component.ts
+++ src/app/components/poke-table/poke-table.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-poke-table',
   templateUrl: './poke-table.component.html',
   styleUrls: ['./poke-table.component.scss']
 })
-export class PokeTableComponent {
+export class PokeTableComponent implements OnInit {
+
+displayedColumns: string[] = ['position', 'image', 'name'];
+data: any [] = [];
+datasource = new MatTableDataSource<any>(this.data);
+
+
+@ViewChild
+
+
+constructor(private PokeService: PokemonService) { 
+
+
+}
+
+ngOnInit(): void {
+    this.getPokemons();
+}
+
+
+getPokemons()
+{
+for(let i = 1; i <= 150; i++) { 
+
+    this.PokeService.getPokemons(i).subscribe (
+      res=> {
+      console.log(res);
+      },
+      err=>{ 
+    
+      }
+    );
+
+}
+}
+
 
 }
