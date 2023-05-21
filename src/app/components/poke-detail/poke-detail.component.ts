29c03dd8dc4f1a017090be0a7a9c9554f56f4305
Sergio <sergio.skate89@gmail.com>
2023-05-21 00:43:12 -0600

    Pokedetails completo

--- src/app/components/poke-detail/poke-detail.component.ts
+++ src/app/components/poke-detail/poke-detail.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {PokemonService} from 'src/app/services/pokemon.service'
 
 @Component({
   selector: 'app-poke-detail',
   templateUrl: './poke-detail.component.html',
   styleUrls: ['./poke-detail.component.scss']
 })
-export class PokeDetailComponent {
+export class PokeDetailComponent implements OnInit {
+
+  pokemon: any = '';
+  pokemonType = [];
+  pokemonImg = '';
+
+  constructor(private pokemonService: PokemonService , private activatedRouter: ActivatedRoute) {
+    this.activatedRouter.params.subscribe(
+      params => {
+        this.getPokemons(params['id'])
+      }
+    )
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  getPokemons(id: any){
+    this.pokemonService.getPokemons(id).subscribe(
+      res => {
+        this.pokemon = res;
+        this.pokemonImg = this.pokemon.sprites.front_default;
+        this.pokemonType = res.types[0].type.name;
+      },
+      err => {
+        console.log(err);
+      }
+
+    )
+  }
 
 }
